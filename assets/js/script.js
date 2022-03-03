8a79a2a7ee4ecb7e46833c1ad7481650b0bd2177
fionatreacy <fionatreacy00@gmail.com>
2022-03-03 14:20:31 +0000

    add code to incrememnt scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,8 +36,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incremementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -57,12 +59,14 @@ function calculateCorrectAnswer() {
 }
 
 function incrementScore() {
-
+let oldScore = parseInt(document.getElementById("score").innerText);
+document.getElementById("score").innerText = ++oldScore;
 };
 
 function incremementWrongAnswer() {
-
-}
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+    };
 
 function displayAdditionQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
